d415ff13ef16f8398f98be64017215921ec4952d
lanboys <lan_bing2013@163.com>
2019-01-09 11:39:23 +0800

    初始化web项目

---
+++ src/main/java/com/bing/lan/utils/JavaUtilsApplication.java
@@ -0,0 +1,14 @@
+package com.bing.lan.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilsApplication.class, args);
+	}
+
+}
+
